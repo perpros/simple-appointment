6f5a9cb9b4115616837e840b2a783946e85ab569
“Majid <majidfathi69@gmail.com>
2024-07-18 17:04:18 +0330

    wrote definition of ServiceWidget without implemention

---
+++ lib/presentation/widgets/service_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../view_model/service_view_model.dart';
+
+class ServiceWidget extends StatelessWidget {
+  const ServiceWidget({Key? key, required this.service}) : super(key: key);
+
+  final ServiceViewModel service;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
