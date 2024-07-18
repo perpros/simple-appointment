f4f0845cbe14462b99fedcd7ab66c9becf000aab
“Majid <majidfathi69@gmail.com>
2024-07-18 17:05:15 +0330

    wrote ServiceViewModel

---
+++ lib/presentation/view_model/service_view_model.dart
@@ -0,0 +1,15 @@
+class ServiceViewModel {
+  ServiceViewModel(
+    this.name,
+    this.details,
+    this.description,
+    this.stars,
+    this.pictureUrl,
+  );
+
+  final String name;
+  final String details;
+  final String description;
+  final double stars;
+  final String? pictureUrl;
+}
