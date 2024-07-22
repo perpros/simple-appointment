eca481104023cce30b6f254686b1c0aabf807c11
“Majid <majidfathi69@gmail.com>
2024-07-22 17:18:23 +0330

    wrote definition of FillAppointmentView

---
+++ lib/presentation/view/fill_appointment/appointment_for_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppointmentForWidget extends StatelessWidget {
+  const AppointmentForWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/presentation/view/fill_appointment/fill_appointment_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../view_model/patient_view_model.dart';
+import '../../view_model/service_view_model.dart';
+import '../../view_model/appointment_for_view_model.dart';
+// import '../../widgets/patient_widget.dart';
+// import '../../widgets/service_widget.dart';
+// import 'appointment_for_widget.dart';
+
+class FillAppointmentView extends StatelessWidget {
+  const FillAppointmentView(
+      {Key? key, required this.service, required this.patiens})
+      : super(key: key);
+
+  final ServiceViewModel service;
+  final List<PatientViewModel> patiens;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  void onNext(AppointmentForViewModel appointmentFor) {}
+  void onPatientSelected(PatientViewModel patientViewModel) {}
+}
