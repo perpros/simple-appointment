5489eba04dab4feb1417bfb6cbe9cf99dabb7a3b
“Majid <majidfathi69@gmail.com>
2024-07-22 17:13:47 +0330

    wrote PatientWidget

---
+++ lib/presentation/widgets/patient_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../view_model/patient_view_model.dart';
+
+class PatientWidget extends StatelessWidget {
+  const PatientWidget({Key? key, required this.patient}) : super(key: key);
+
+  final PatientViewModel patient;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
