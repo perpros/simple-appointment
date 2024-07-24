3aceb36d5118a1c670d94540baaffad2166512a5
“Majid <majidfathi69@gmail.com>
2024-07-24 11:14:59 +0330

    wrote definition of ConfirmAppointmentView

---
+++ lib/presentation/view/confirm_appointment/confirm_appointment_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../view_model/appointment_for_view_model.dart';
+import '../../view_model/service_view_model.dart';
+import '../../view_model/time_slot_view_model.dart';
+
+class ConfirmAppointmentView extends StatelessWidget {
+  const ConfirmAppointmentView(
+      {Key? key,
+      required this.appointmentFor,
+      required this.service,
+      required this.timeSlot})
+      : super(key: key);
+
+  final ServiceViewModel service;
+  final AppointmentForViewModel appointmentFor;
+  final TimeSlotViewModel timeSlot;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  void doConfirm() {}
+}
