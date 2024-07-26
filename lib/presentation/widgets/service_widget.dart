9915e5338b5933544691ab8878059f213acbc98f
“Majid <majidfathi69@gmail.com>
2024-07-26 14:16:51 +0330

    implement ServiceWidget

--- lib/presentation/widgets/service_widget.dart
+++ lib/presentation/widgets/service_widget.dart
@@ -9,7 +9,22 @@ class ServiceWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Row(
+      children: [
+        const SizedBox(width: 80, height: 80),
+        Column(
+          children: [
+            Row(
+              children: [
+                Text(service.name),
+                Text(service.stars.toString()),
+              ],
+            ),
+            Text(service.details),
+            Text(service.description),
+          ],
+        ),
+      ],
+    );
   }
 }
